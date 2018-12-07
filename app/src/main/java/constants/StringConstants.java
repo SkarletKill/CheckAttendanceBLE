cdbe74c82c404b1394327b43158c0bb903175b9f
NeO <Skarletvlad@gmail.com>
2018-12-07 16:45:03 +0200

    - added prototype of authorization screen

---
+++ app/src/main/java/constants/StringConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public interface StringConstants {
+    String EMPTY = "";
+    String LOGIN_TEACHER = "t";
+    String LOGIN_STUDENT = "s";
+
+    String WRONG_LOGIN_OR_PASS = "Неправильний логін чи пароль";
+}
--- app/src/main/java/networks/neo/ble/checkattendanceble/AuthorizationActivity.java
+++ app/src/main/java/networks/neo/ble/checkattendanceble/AuthorizationActivity.java
@@ -2,12 +2,48 @@ package networks.neo.ble.checkattendanceble;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import constants.StringConstants;
 
 public class AuthorizationActivity extends AppCompatActivity {
+    Button btn_log;
+    EditText login, pass;
+
+    StringConstants SC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization);
+        init();
+        login();
+    }
+
+    private void init() {
+        login = findViewById(R.id.login);
+        pass = findViewById(R.id.password);
+        btn_log = findViewById(R.id.btnLogin);
+    }
+
+    public void login() {
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (String.valueOf(login.getText()).equals(SC.LOGIN_TEACHER) && String.valueOf(pass.getText()).equals(SC.EMPTY)) {
+                    // goto teacher page
+                } else if(String.valueOf(login.getText()).equals(SC.LOGIN_STUDENT) && String.valueOf(pass.getText()).equals(SC.EMPTY)) {
+                    // goto student page
+                } else {
+                    Toast.makeText(
+                            AuthorizationActivity.this, SC.WRONG_LOGIN_OR_PASS,
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            }
+        });
     }
 }
