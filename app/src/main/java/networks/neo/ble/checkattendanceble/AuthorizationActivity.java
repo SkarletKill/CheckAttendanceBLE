32affef49615591a8b38fc458578b5fa6d0c63e8
NeO <Skarletvlad@gmail.com>
2018-12-07 15:20:04 +0200

    Initial commit

---
+++ app/src/main/java/networks/neo/ble/checkattendanceble/AuthorizationActivity.java
@@ -0,0 +1,13 @@
+package networks.neo.ble.checkattendanceble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
