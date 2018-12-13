9249e7910ab8aa9847bfffea6d10259a30b97864
NeO <Skarletvlad@gmail.com>
2018-12-14 01:12:57 +0200

    - StudentActivity -> UserActivity

--- app/src/main/java/networks/neo/ble/checkattendanceble/UserActivity.java
+++ app/src/main/java/networks/neo/ble/checkattendanceble/UserActivity.java
@@ -2,8 +2,10 @@ package networks.neo.ble.checkattendanceble;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,12 +15,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "StudentDB";
+    private DatabaseReference dbRef;
+
+    private ImageView imgButton;
+    private TextView textImgDescibe;
+    private boolean isSwitchOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +50,9 @@ public class UserActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        init();
+        start();
     }
 
     @Override
@@ -84,5 +102,56 @@ public class UserActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void init() {
+        imgButton = findViewById(R.id.imgPower);
+        textImgDescibe = findViewById(R.id.textOfButtonS);
+        isSwitchOn = false;
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        dbRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Long position = (Long) dataSnapshot.child("position").getValue();
+                String name = (String) dataSnapshot.child("name").getValue();
+//                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Student position is: " + position);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.");
+            }
+        });
+    }
+
+    private void testFDB() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+
+    public void start() {
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isSwitchOn) {
+                    // stop beacon
+                    imgButton.setImageResource(R.drawable.power_button_red_white_inner);
+                    textImgDescibe.setText(R.string.power_on);
+                    isSwitchOn = false;
+                } else {
+                    // start beacon
+                    imgButton.setImageResource(R.drawable.power_button_green_white_inner);
+                    textImgDescibe.setText(R.string.power_off);
+                    isSwitchOn = true;
+                }
+            }
+        });
     }
 }
